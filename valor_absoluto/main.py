1842fdc8ce1e0217eeb2f33387085312ec846ff8
julian-velez <60607869+julian-velez@users.noreply.github.com>
2025-10-23 09:05:33 -0500

    valor_absoluto_math_python
    valor_absoluto_math_python

---
+++ valor_absoluto/main.py
@@ -0,0 +1,7 @@
+import math  # Importa el módulo 'math' que contiene funciones matemáticas avanzadas
+
+# La función math.fabs(x) devuelve el valor absoluto de x como número de tipo float
+# Es decir, convierte un número negativo en positivo, pero siempre retorna un float
+
+print(math.fabs(-10))  # Imprime el valor absoluto de -10 → resultado: 10.0
+
